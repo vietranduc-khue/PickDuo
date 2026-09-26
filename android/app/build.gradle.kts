842636ff2a7a88870836f64eda27e775c7f21283
VT hyper casual games studio <vietranduc@gmail.com>
2026-09-26 15:58:57 +0700

    Raise Android minimum SDK to 24

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     defaultConfig {
         applicationId = "com.vthypercasual.pickduo"
-        minSdk = 21
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
